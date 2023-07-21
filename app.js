a40ff4766103fc3f5377d022276ec3073d9ceab3
bbandhu <bandhavi.b12@gmail.com>
2023-07-20 19:24:11 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,87 +1,76 @@
-// Create an array of objects each containing a question and the possible answers
-// We assume 'a' is the correct answer here. You may adjust as needed.
-var questions = [
-  {question: "Question 1?", answers: ["a", "b", "c", "d"], correct: "a"},
-  {question: "Question 2?", answers: ["a", "b", "c", "d"], correct: "b"},
-  {question: "Question 3?", answers: ["a", "b", "c", "d"], correct: "c"},
-  //... add as many questions as needed
+let quizData = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
 ];
 
-var score = 0;
-var currentQuestion = 0;
-var timeLeft = 100; // Start timer at 100 seconds, adjust as needed
-var timer;
+let currentQuestionIndex = 0;
 
-// Start the quiz when the start button is clicked
-document.getElementById("start").addEventListener("click", function() {
-  document.querySelector(".title").classList.add("hide");
-  document.getElementById("questions").classList.remove("hide");
+const startButton = document.getElementById("start");
+const questionContainer = document.getElementById("question-container");
+const questionDiv = document.createElement("div");
+const questionHeader = document.createElement("h2");
+const choicesList = document.createElement("ul");
 
-  timer = setInterval(function() {
-    timeLeft--;
-    document.getElementById("time").textContent = timeLeft;
+questionDiv.appendChild(questionHeader);
+questionDiv.appendChild(choicesList);
+questionContainer.appendChild(questionDiv);
 
-    if(timeLeft <= 0) {
-      endGame();
-    }
-  }, 1000);
-
-  displayQuestion();
-});
-
-// Display the current question
 function displayQuestion() {
-  var question = questions[currentQuestion];
-  document.getElementById("question-title").textContent = question.question;
-  
-  var choicesDiv = document.getElementById("choices");
-  choicesDiv.innerHTML = ''; // Clear out any previous choices
+  let currentQuestion = quizData[currentQuestionIndex];
+  questionHeader.textContent = `Question ${currentQuestionIndex + 1}: ${currentQuestion.question}`;
 
-  // Display the choices and add click handlers for them
-  question.answers.forEach(function(choice, i) {
-    var button = document.createElement("button");
-    button.textContent = choice;
-    button.onclick = function() {
-      checkAnswer(choice);
-    };
-
-    choicesDiv.appendChild(button);
+  choicesList.innerHTML = ''; // Clear out any previous choices
+  currentQuestion.choices.forEach((choice) => {
+    const choiceItem = document.createElement("li");
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceItem.appendChild(choiceButton);
+    choiceButton.addEventListener('click', () => {
+      currentQuestionIndex++;
+      if (currentQuestionIndex < quizData.length) {
+        displayQuestion();
+      }
+      else {
+        // end the quiz or do something else
+        console.log("Quiz finished!");
+      }
+    });
+    choicesList.appendChild(choiceItem);
   });
 }
 
-// Check the answer, update the score if correct, decrease time if incorrect
-function checkAnswer(answer) {
-  if(answer === questions[currentQuestion].correct) {
-    score++;
-  } else {
-    timeLeft -= 15; // Subtract 15 seconds for a wrong answer, adjust as needed
-  }
-
-  if(currentQuestion < questions.length - 1) {
-    currentQuestion++;
-    displayQuestion();
-  } else {
-    endGame();
-  }
-}
-
-// End the game, stop the timer, allow the user to save their score
-function endGame() {
-  clearInterval(timer);
-  document.getElementById("questions").classList.add("hide");
-  document.getElementById("end-screen").classList.remove("hide");
-  document.getElementById("final-score").textContent = score;
-}
-
-// Save the score and initials to local storage
-document.getElementById("submit").addEventListener("click", function() {
-  var initials = document.getElementById("initials").value;
-  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-  var newScore = {
-    initials: initials,
-    score: score
-  };
-
-  highScores.push(newScore);
-  localStorage.setItem("highScores", JSON.stringify(highScores));
+startButton.addEventListener("click", () => {
+  startButton.style.display = 'none'; // Hide start button
+  displayQuestion();
 });
